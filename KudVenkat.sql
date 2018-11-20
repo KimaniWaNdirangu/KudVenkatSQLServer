cc7f37a2c56cc1b4b4435a17ba95d7dfbb7b75fe
KimaniWaNdirangu <kimani.wa.ndirangu@gmail.com>
2018-11-20 08:12:34 +0300

    Added Part 4 - Default Contraints

--- KudVenkat.sql
+++ KudVenkat.sql
@@ -30,8 +30,78 @@ SELECT * FROM tblGender
 SELECT * FROM tblPerson
 
 -- Creating Foreign Key Constraints
+------------------------------------
 -- A Foreign Key in one table points to a Primary Key in another table
 
 ALTER TABLE tblPerson
 ADD CONSTRAINT tblPerson_GenderID_FK
 FOREIGN KEY(GenderID) REFERENCES tblGender(ID)
+
+-- Practice
+----------------------------------------------
+CREATE DATABASE CreditCollect
+
+USE CreditCollect
+GO
+
+CREATE TABLE Gender
+(
+	ID INT NOT NULL PRIMARY KEY,
+	Name NVARCHAR(50)
+);
+
+SELECT * FROM Defaulter
+SELECT * FROM Gender
+
+ALTER TABLE Defaulter
+ADD CONSTRAINT FK_Defaulter_GenderID
+FOREIGN KEY (GenderID) REFERENCES Gender(ID); 
+
+-- End Practice
+---------------------------------------------
+
+-- Part 4 - DEFAULT CONSTRAINT
+
+SELECT * FROM tblGender
+SELECT * FROM tblPerson
+
+INSERT INTO tblGender (ID, Gender)
+VALUES (1, 'Male')
+
+INSERT INTO tblGender (ID, Gender)
+VALUES (2, 'Female')
+
+INSERT INTO tblGender (ID, Gender)
+VALUES (3, 'Unknown')
+-- -----------------------------------------------
+
+INSERT INTO tblPerson (ID, Name, Email, GenderID)
+VALUES (3, 'Simon', 's@s.com', 1)
+
+INSERT INTO tblPerson (ID, Name, Email, GenderID)
+VALUES (4, 'Sam', 'sam@sam.com', 1)
+
+INSERT INTO tblPerson (ID, Name, Email, GenderID)
+VALUES (5, 'May', 'may@may.com', 2)
+
+INSERT INTO tblPerson (ID, Name, Email, GenderID)
+VALUES (6, 'Kenny', 'k@k.com', 3)
+
+INSERT INTO tblPerson (ID, Name, Email)
+VALUES (7, 'Rich', 'r@r.com')
+
+ALTER TABLE tblPerson
+ADD CONSTRAINT DF_tblPerson_GenderID
+DEFAULT 3 FOR GenderID
+
+INSERT INTO tblPerson (ID, Name, Email)
+VALUES (8, 'Mike', 'mike@r.com')
+
+INSERT INTO tblPerson (ID, Name, Email, GenderID)
+VALUES (9, 'Sara', 's@r.com', 2)
+
+INSERT INTO tblPerson (ID, Name, Email, GenderID)
+VALUES (10, 'Johnny', 'j@r.com', NULL)
+
+ALTER TABLE tblPerson
+DROP CONSTRAINT DF_tblPerson_GenderID
